6cad8480a5fc19633e0afc4d673085d26d2b6861
jk <joachim.dev@pm.me>
2024-10-28 15:34:13 +0100

    update colorscheme definition to catppuccin

--- modules/nixvim/colorscheme/default.nix
+++ modules/nixvim/colorscheme/default.nix
@@ -1,28 +1,13 @@
-{ helpers, ... }:
+{ ... }:
 {
-  extraConfigLuaPost = # lua
-    ''
-      do
-        -- The NixVim Nord colorscheme doesn't assign a pleasing color to the window
-        -- separator by default. Here we override it with a softer color.
-        local colors = require("nord.colors")
-        vim.api.nvim_set_hl(0,  "WinSeparator", {
-          fg = colors.nord2_gui,
-        })
-        vim.api.nvim_set_hl(0,  "LspInlayHint", {
-          fg = colors.nord3_gui,
-        })
-      end
-    '';
-
-
-  colorschemes.nord = {
+  colorschemes.catppuccin = {
     enable = true;
     settings = {
-      borders = true;
-      contrast = false;
-      disable_background = false;
-      enable_sidebar_background = true;
+      flavour = "auto";
+      background = {
+        dark = "mocha";
+        light = "latte";
+      };
     };
   };
 }
